3aac4ceaba7322a34d657af5a4755cca30b92539
github username <github email address>
2019-06-03 20:49:40 -0400

    final lab solution

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -16,134 +16,102 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    gets.chomp
-    gets.chomp
-    gets.chomp
-    gets.chomp
-  end
-  
-  def list_songs
-    counter = 0
-    song_library = Song.all.collect do |song|
-      [song.artist.name, song.name, song.genre.name]
-    end
-
-    sorted = song_library.sort {|a, b| a[1][0] <=> b[1][0]}
-    
-    binding.pry
-    numbered = sorted.collect do |song|
-      counter += 1
-      ["#{counter}. " + song[0], song[1..-1]].flatten
-    end
-
-    final_list = numbered.collect do |song|
-      song.join(" - ")
-    end
-
-    final_list.each do |song|
-      puts song
-    end
-    
-  
-  end
-  
-  def list_artists
-    counter = 0
-    artists = Artist.all.collect do |artist|
-      artist.name
-    end
-    
-    #artists is an array of artist names
-    sorted_artists = artists.sort {|a, b| a <=> b}
-    
-    numbered_artists =
-      sorted_artists.collect do |artist|
-        counter += 1 
-        artist = "#{counter}. " + artist
-      end
-
-    numbered_artists.each do |artist|
-      puts artist
-    end
-
-  end
-  
-  def list_genres
-    genres = Genre.all.collect do |genre|
-      genre.name
-    end
-    counter = 0
-    
-    sorted_genres = genres.sort {|a, b| a <=> b}
-    numbered_genres =
-      sorted_genres.collect do |genre|
-        counter +=1
-        genre = "#{counter}. " + genre
-      end
-
-    numbered_genres.each do |genre|
-      puts genre
-    end
-  end
-  
-  def list_songs_by_artist
-    puts "Please enter the name of an artist:"
-    artist = gets.chomp
-    counter = 1
-    
-    songs_array = Song.all.collect do |song|
-      [song.name, song.artist, song.genre]
-    end
-
       
-    # songs_array = @importer.collect do |song|
-    #   song.split(" - ")
-    # end
-  
-    
-    # returns a nested array of all songs by artist
-    by_artist = songs_array.select {|song| song[0] == artist}
-    
-    # removes the artist from the beginning of each song array
-    removed_artist =
-      by_artist.collect do |song|
-        song.drop(1)
+      input = nil
+      
+      until input == "exit"
+        input = gets.chomp
+        case input
+        when "list songs" then list_songs
+        when "list artists" then list_artists
+        when "list genres" then list_genres
+        when "list artist" then list_songs_by_artist
+        when "list genre" then list_songs_by_genre
+        when "play song" then play_song
+        end
       end
-    
-    # sorted is an array of song arrays
-    sorted = removed_artist.sort {|a, b| a <=> b}
-    
-    # adds 1-4 to the beginning of each song array
-    sorted.collect do |song|
-      song.unshift("#{counter}.")
-      counter += 1
-    end
-    
-    # removes mp3 from the end, combines counter with song[1]
-    nompthree = sorted.collect do |song|
-      song[2].gsub!(/(.mp3)/, "")
-      song[1] = song[0] + " " + song[1]
-      song
-    end
-  
-    # drops the first item from the array (the extra number)
-    dropped = nompthree.collect do |song|
-      song.drop(1)
-    end
-    
-    # adds the hyphen back in
-    withhyphen = dropped.collect do |song|
-      song.join(" - ")
-    end
-    
-    # puts each song out
-    withhyphen.each do |song|
-      puts song
-    end
-    
-    
 
+    end
+    
+    def list_songs
+    
+      
+      sorted = Song.all.sort_by {|song| song.name}
+      
+      sorted.map.with_index do |song, i|
+        puts "#{i+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+        song
+      end
+    end
+    
+    def list_artists
+      
+      sorted = Artist.all.sort_by {|artist| artist.name}
+      
+      sorted.map.with_index do |artist, i|
+        puts "#{i+1}. #{artist.name}"
+      end
+      
+    end
+    
+    def list_genres
+      genres = Genre.all.collect do |genre|
+        genre
+      end
+      
+      sorted = genres.sort_by {|genre| genre.name}
+      
+      sorted.map.with_index do |genre, i|
+        puts "#{i+1}. #{genre.name}"
+      end
+    end
+    
+    def list_songs_by_artist
+      puts "Please enter the name of an artist:"
+      artist_name = gets.chomp
+      counter = 0
+      
+      songs_by_artist = Song.all.select do |song|
+        if (song.artist.name == artist_name) 
+          song
+        end
+      end
+      
+      sorted = songs_by_artist.sort_by {|song| song.name}
+      
+      sorted.map.with_index do |song, i|
+        puts "#{i+1}. #{song.name} - #{song.genre.name}"
+      end
+    end
+    
+    
+    def list_songs_by_genre
+      puts "Please enter the name of a genre:"
+      genre_name = gets.chomp
+      
+      songs_in_genre = Song.all.select do |song|
+        if (song.genre.name == genre_name) 
+          song
+        end
+      end
+      
+      sorted = songs_in_genre.sort_by {|song| song.name}
+      
+      sorted.map.with_index do |song, i|
+        puts "#{i+1}. #{song.artist.name} - #{song.name}"
+      end
+    end
+
+    def play_song
+      puts "Which song number would you like to play?"
+      song_number = gets.to_i
+      
+      sorted = Song.all.sort_by {|song| song.name}
+
+      if song_number.between?(1, sorted.length)
+        puts "Playing #{sorted[song_number-1].name} by #{sorted[song_number-1].artist.name}"
+
+      end
+    end
   end
-
-    
-end
+  
